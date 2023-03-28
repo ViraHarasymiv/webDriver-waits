d0a8911b878ffd5e9cadd2175e82c848913327b2
Harasymiv <vira.harasymiv@nung.edu.ua>
2023-03-28 09:07:24 +0300

    description added

--- src/test/java/com/softserve/edu/TableTest.java
+++ src/test/java/com/softserve/edu/TableTest.java
@@ -1,13 +1,13 @@
 package com.softserve.edu;
 
-import org.asynchttpclient.util.Assertions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -18,19 +18,28 @@ public class TableTest extends BaseTest{
 
     @Test
     public void checkCityPresence(){
+        //Click on the Understand button
         driver.findElement(By.xpath("//button[contains(text(), 'Understand')]")).click();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
         wait.until(ExpectedConditions
                 .invisibilityOfElementLocated(By.xpath("//footer[contains(@class, 'pJvQ8')]")));
         driver.switchTo().frame(driver.findElement(By.xpath("//div[contains(@data-options, '/filter-row')]//iframe")));
+        //Scroll to the Uncontrolled Mode item table
+        Actions actions = new Actions(driver);
+        actions.moveToElement(
+                driver.findElement(By.xpath("//div[contains(@data-options, '/filter-row')]//th[3]//input[contains(@placeholder, 'Filter')]")))
+                .perform();
+        //Enter the letter L in the City filter
         List<WebElement>initialElements = driver.findElements(By.xpath("//div[contains(@data-options, '/filter-row')]//tbody//td"));
         driver.findElement(By
-                .xpath("//div[contains(@data-options, '/filter-row')]//th[3]//input[contains(@placeholder, 'Filter')]")).sendKeys("L");
+                .xpath("//div[contains(@data-options, '/filter-row')]//th[3]//input[contains(@placeholder, 'Filter')]"))
+                .sendKeys("L", Keys.ENTER);
         waitForTableSizeChanging(initialElements);
-        List<WebElement>labels = driver.findElements(By.xpath("//div[contains(@data-options, '/filter-row')]//tbody//td"));
+        List<WebElement> elements = driver.findElements(By.xpath("//div[contains(@data-options, '/filter-row')]//tbody//td"));
+        //Check the presence in the column of the cities of Las Vegas and London
         SoftAssert softAssert = new SoftAssert();
-        softAssert.assertTrue(labels.stream().anyMatch(city -> city.getText().contains("Las Vegas")));
-        softAssert.assertTrue(labels.stream().anyMatch(city -> city.getText().contains("London")));
+        softAssert.assertTrue(elements.stream().anyMatch(city -> city.getText().contains("Las Vegas")));
+        softAssert.assertTrue(elements.stream().anyMatch(city -> city.getText().contains("London")));
         softAssert.assertAll();
     }
 
